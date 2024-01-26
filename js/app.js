0aeafc4b09b5e6a9afa7a1e0868769150ed50325
preveira <patrick.k.reveira@gmail.com>
2024-01-26 14:15:31 -0800

    completed sum

--- js/app.js
+++ js/app.js
@@ -9,14 +9,15 @@ element is a concatenated string that EXACTLY follows this example and uses the 
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-// Write your code here
-function sum(a, b) { //eslint-disable-line
-        let number = a + b;
-        return[a+b, `the sum of ${a} and ${b} is ${number}`];
-}
+ // Write your code here
+        function sum(a, b) { //eslint-disable-line
+            let number = a + b;
+            return[a + b, `The sum of ${a} and ${b} is ${number}.`];
+        }
+
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
